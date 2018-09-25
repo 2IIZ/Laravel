e1ce62c97446f24a57a2d1c9772d36de0d52d563
Unknown <2IIZ1998@gmail.com>
2018-09-25 10:43:00 +0200

    Added tables bdd + (fr) for registering typos

--- GalleryProject/config/app.php
+++ GalleryProject/config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'GalleryProject'),
 
     /*
     |--------------------------------------------------------------------------
@@ -77,7 +77,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'fr',
 
     /*
     |--------------------------------------------------------------------------
