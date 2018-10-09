2b85d9521fc1eff9bf6ed9dd169e8efdd57523c9
Unknown <2IIZ1998@gmail.com>
2018-10-09 11:42:59 +0200

    Added "Line codes" (commands line that I use) + Creation of middleware

--- GalleryProject/app/Providers/AppServiceProvider.php
+++ GalleryProject/app/Providers/AppServiceProvider.php
@@ -15,6 +15,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Blade::if('admin', function () {
+          Return auth()->check()&&auth()->user()->role==='admin';
+        });
     }
 
     /**
