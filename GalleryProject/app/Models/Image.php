3c0c0011b6c3dcdf3cbf44def9c430015787f595
Unknown <2IIZ1998@gmail.com>
2018-10-16 09:49:38 +0200

    Created new project cms
    "following course"

--- GalleryProject/app/Models/Image.php
+++ GalleryProject/app/Models/Image.php
@@ -1,4 +1,9 @@
 <?php
+# @Date:   2018-10-02T10:41:57+02:00
+# @Last modified time: 2018-10-16T09:05:27+02:00
+
+
+
 
 //To create this : "php artisan make:model Image"
 
@@ -12,6 +17,7 @@ class Image extends Model
 
   public function user() {
       return $this->belongsTo('App\Models\User');
+      
   }
 
 }
