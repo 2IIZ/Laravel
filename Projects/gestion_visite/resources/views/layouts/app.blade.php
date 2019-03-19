cb13e20cbd10876bb33b04e00a43604aacea7179
Unknown <2IIZ1998@gmail.com>
2019-03-19 16:36:42 +0100

    navbar added

--- Projects/gestion_visite/resources/views/layouts/app.blade.php
+++ Projects/gestion_visite/resources/views/layouts/app.blade.php
@@ -23,17 +23,30 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
+
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
+                    <ul class="navbar-nav mr-auto ml-3">
+                      <li class="nav-item">
+                        <a class="nav-link" href="{{ route('home') }}">{{ __('Accueil') }}</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="{{ route('home') }}">{{ __('Mes rapports') }}</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="{{ route('home') }}">{{ __('autres') }}</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="{{ route('calendar') }}">{{ __('Mon Calendrier') }}</a>
+                      </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -48,7 +61,7 @@
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
-                        @else
+                            @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
